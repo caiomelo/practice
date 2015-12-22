25f577f77a673cbf2bc68ab88b668ce9d504ac92
Caio Melo <caio.compufc@gmail.com>
2015-12-22 00:43:20 -0300

    - Add bootstrap's navbar - Add listener for the changes in the editor's text - Add html interpreter to the editor

--- coursera/web-app-dev-js-mongodb/textcircle/textcircle.js
+++ coursera/web-app-dev-js-mongodb/textcircle/textcircle.js
@@ -2,16 +2,6 @@ this.Documents = new Mongo.Collection("documents");
 
 if (Meteor.isClient) {
 
-  //Sets an interval of 1000ms between the executions of the specified function
-  Meteor.setInterval(setCurrentDate, 1000);
-
-
-  Template.date_display.helpers({
-    current_date:function() {
-      return Session.get("current_date");
-    }
-  });
-
   Template.editor.helpers({
     docid:function() {
       var doc = Documents.findOne();
@@ -20,7 +10,12 @@ if (Meteor.isClient) {
       } else {
         return undefined;
       }
-    }
+    },
+
+    config:function() {
+      return addListener;
+    },
+
   });
 
 }
@@ -29,15 +24,20 @@ if (Meteor.isServer) {
   Meteor.startup(createNewDoc);
 }
 
-  //Sets the value of the current_date session variable with the current date and time
-  function setCurrentDate() {
-      Session.set("current_date", new Date());
+//Creates a new document to be edited, if it doesn't exist yet
+function createNewDoc() {
+  if(!Documents.findOne()) {
+    var newDocument = {title: "new doc", author:"Caio"};
+    Documents.insert(newDocument);
   }
+}
 
-  //Creates a new document to be edited, if it doesn't exist yet
-  function createNewDoc() {
-    if(!Documents.findOne()) {
-      var newDocument = {title: "new doc", author:"Caio"};
-      Documents.insert(newDocument);
-    }
-  }
+//Adds a listener for the editor's changes
+function addListener(editor) {
+  editor.on("change", showCurrValue);
+}
+
+//Gets the content of the preview_iframe and adds the cm_editor content to it as html
+function showCurrValue(cm_editor, info) {
+  $("#preview_iframe").contents().find("html").html(cm_editor.getValue());
+}
